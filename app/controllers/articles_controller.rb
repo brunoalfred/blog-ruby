e846b5a0d7bec5d2767b68c4f4f08af2d4915dfc
bruno alfred <alfredybruno@gmail.com>
2020-11-03 11:45:19 +0300

    Form submitts and Shows The Submittion :+1: :100:

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,25 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+      end
+
+
     def new
     end
 
-    def create
-            render plain: params[:article].inspect          
-    end
+    
+            # render plain: params[:article].inspect          
+
+            def create
+                @article = Article.new(article_params)
+               
+                @article.save
+                redirect_to @article
+              end
+               
+              private
+                def article_params
+                  params.require(:article).permit(:title, :text)
+                end
+
 end
