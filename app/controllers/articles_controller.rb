8b09e7eb28d9fcf8e21319c023e92cde6d131a8c
bruno alfred <alfredybruno@gmail.com>
2020-11-03 12:51:31 +0300

    Added Editng and Updating Actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,19 +9,34 @@ class ArticlesController < ApplicationController
       end
 
     def new
+      @article = Article.new
     end
 
+    def edit
+      @article = Article.find(params[:id])
+    end
     
-            # render plain: params[:article].inspect          
+   # render plain: params[:article].inspect          
+   def create
+    @article = Article.new(article_params)
+   
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end 
 
-            def create
-                @article = Article.new(article_params)
-               
-                @article.save
-                redirect_to @article
-              end
-               
-              private
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+            private
                 def article_params
                   params.require(:article).permit(:title, :text)
                 end
