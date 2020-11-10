0924300d197874c1289d67dcc0378278a5e1446a
bruno alfred <alfredybruno@gmail.com>
2020-11-10 16:44:51 +0300

    importing js file to be used by the app

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+require("packs/scripts")
+console.log('Hello from application.js')
+
+//= require jquery3
+//= require popper
+//= require bootstrap
